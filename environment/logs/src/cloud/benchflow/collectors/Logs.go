98ef1188d6201904f1f3ef37f7c13f51d6f82bc5
Cerfoglg <gabrielecerfoglio225@gmail.com>
2015-10-20 18:02:06 +0200

    Changed Minio buckets and filenames

--- environment/logs/src/cloud/benchflow/collectors/Logs.go
+++ environment/logs/src/cloud/benchflow/collectors/Logs.go
@@ -64,7 +64,7 @@ func collectStats(container Container) {
 		object.Close()
 		log.Fatalln(err)
 	}
-	err = s3Client.PutObject("data", container.ID+"_tmp.7z", "application/octet-stream", objectInfo.Size(), object)
+	err = s3Client.PutObject("benchmarks/a/runs/1", os.Getenv("CONTAINER_NAME")+"_Logs.7z", "application/octet-stream", objectInfo.Size(), object)
 	if err != nil {
 		log.Fatalln(err)
 	}
