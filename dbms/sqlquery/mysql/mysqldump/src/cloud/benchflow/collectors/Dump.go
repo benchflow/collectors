f96cdf644096d58638110734f604882e9acdfb5e
Cerfoglg <gabrielecerfoglio225@gmail.com>
2015-12-05 15:56:53 +0100

    Using commons libraries

--- dbms/sqlquery/mysql/mysqldump/src/cloud/benchflow/collectors/Dump.go
+++ dbms/sqlquery/mysql/mysqldump/src/cloud/benchflow/collectors/Dump.go
@@ -5,49 +5,11 @@ import (
     "net/http"
     "os"
     "os/exec"
-    "github.com/minio/minio-go"
-    "log"
+    //"github.com/minio/minio-go"
+    "github.com/Cerfoglg/commons/src/minio"
+    //"log"
     "strings"
 )
-
-func generateKey(fileName string) string{
-	return ("hash/BID/1/"+os.Getenv("CONTAINER_NAME")+"/"+os.Getenv("COLLECTOR_NAME")+"/"+os.Getenv("DATA_NAME")+"/"+fileName)
-}
-
-func gzipFile(fileName string) {
-	cmd := exec.Command("gzip", fileName)
-	err := cmd.Start()
-	cmd.Wait()
-	if err != nil {
-		panic(err)
-		}
-	}
-
-func storeOnMinio(fileName string, bucket string, key string) {
-	config := minio.Config{
-		AccessKeyID:     os.Getenv("MINIO_ACCESS_KEY_ID"),
-		SecretAccessKey: os.Getenv("MINIO_SECRET_ACCESS_KEY"),
-		Endpoint:        os.Getenv("MINIO_HOST"),
-		}
-		s3Client, err := minio.New(config)
-	    if err != nil {
-	        log.Fatalln(err)
-	    }  
-	    object, err := os.Open(fileName)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer object.Close()
-		objectInfo, err := object.Stat()
-		if err != nil {
-			object.Close()
-			log.Fatalln(err)
-		}
-		err = s3Client.PutObject(bucket, key, "application/octet-stream", objectInfo.Size(), object)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
 
 func backupHandler(w http.ResponseWriter, r *http.Request) {
 	// Retrieve table names
@@ -73,8 +35,8 @@ func backupHandler(w http.ResponseWriter, r *http.Request) {
 	        panic(err)
 	    }
 	    outfile.Close()
-	    gzipFile("backup.csv")
-		storeOnMinio("backup.csv.gz", "runs", generateKey(each+".csv.gz"))
+	    minio.GzipFile("backup.csv")
+		minio.StoreOnMinio("backup.csv.gz", "runs", minio.GenerateKey(each+".csv.gz"))
 	}
     
     // Save the column types of the tables
@@ -96,8 +58,8 @@ func backupHandler(w http.ResponseWriter, r *http.Request) {
 	        panic(err)
 	    }
 	    outfile.Close()
-	    gzipFile("backup.csv")
-		storeOnMinio("backup.csv.gz", "runs", generateKey(each+"_schema.csv.gz"))
+	    minio.GzipFile("backup.csv")
+		minio.StoreOnMinio("backup.csv.gz", "runs", minio.GenerateKey(each+"_schema.csv.gz"))
 	}
     
 	fmt.Fprintf(w, "SUCCESS")
