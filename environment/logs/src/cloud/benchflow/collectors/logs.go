0e6d04643d75cf2425a5fa66f64b344cec4b9e76
Cerfoglg <gabrielecerfoglio225@gmail.com>
2016-02-17 14:47:58 +0100

    Fixed issue with converting string to int

--- environment/logs/src/cloud/benchflow/collectors/logs.go
+++ environment/logs/src/cloud/benchflow/collectors/logs.go
@@ -33,7 +33,8 @@ type KafkaMessage struct {
 	}
 
 func signalOnKafka(minioKey string) {
-	kafkaMsg := KafkaMessage{SUT_name: "Camunda", SUT_version: "", Minio_key: minioKey, Trial_id: os.Getenv("TRIAL_ID"), Experiment_id: os.Getenv("EXPERIMENT_ID"), Total_trials_num: os.Getenv("TOTAL_TRIALS_NUM")}
+	totalTrials := strconv.Atoi(os.Getenv("TOTAL_TRIALS_NUM"))
+	kafkaMsg := KafkaMessage{SUT_name: "Camunda", SUT_version: "", Minio_key: minioKey, Trial_id: os.Getenv("TRIAL_ID"), Experiment_id: os.Getenv("EXPERIMENT_ID"), Total_trials_num: totalTrials}
 	jsMessage, err := json.Marshal(kafkaMsg)
 	if err != nil {
 		log.Printf("Failed to marshall json message")
