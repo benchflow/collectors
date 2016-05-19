d11a800e015f4b39d884adaf003440b0f4116dc3
Gabriele Cerfoglio <gabrielecerfoglio225@gmail.com>
2016-05-19 14:25:57 +0200

    Missing deps for properties (#57)
    * Added missing deps to properties
    
    * Added missing dep

--- environment/properties/Godeps/_workspace/src/github.com/benchflow/commons/minio/minio.go
+++ environment/properties/Godeps/_workspace/src/github.com/benchflow/commons/minio/minio.go
@@ -1,38 +1,10 @@
 package minio
 
 import (
-	"github.com/minio/minio-go"
 	"os"
 	"os/exec"
-	"log"
 	"strings"
 )
-
-func StoreOnMinio(fileName string, bucket string, key string) {
-	config := minio.Config{
-		AccessKeyID:     os.Getenv("MINIO_ACCESSKEYID"),
-		SecretAccessKey: os.Getenv("MINIO_SECRETACCESSKEY"),
-		Endpoint:        os.Getenv("MINIO_HOST"),
-		}
-		s3Client, err := minio.New(config)
-	    if err != nil {
-	        log.Fatalln(err)
-	    }  
-	    object, err := os.Open(fileName)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer object.Close()
-		objectInfo, err := object.Stat()
-		if err != nil {
-			object.Close()
-			log.Fatalln(err)
-		}
-		err = s3Client.PutObject(bucket, key, "application/octet-stream", objectInfo.Size(), object)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
 
 func GenerateKey(fileName string) string {
 	// TODO: Use the hash library to generate a hash
