ed7b2807f96239b885f1cf20fff5d22cd3d5d9c8
Gabriele Cerfoglio <gabrielecerfoglio225@gmail.com>
2016-06-17 08:47:50 +0200

    Commons for zip collector (#71)
    * Minio API, commons
    
    * deps
    
    * kafka from commons in makefile
    
    * port added to minio call

--- files/zip/src/cloud/benchflow/collectors/zip.go
+++ files/zip/src/cloud/benchflow/collectors/zip.go
@@ -5,53 +5,15 @@ import (
     "net/http"
     "os"
     "os/exec"
-    "encoding/json"
-    "log"
     "strings"
-    "bytes"
     "github.com/benchflow/commons/minio"
-    "github.com/Shopify/sarama"
-    "strconv"
+    "github.com/benchflow/commons/kafka"
 )
-
-type KafkaMessage struct {
-	SUT_name string `json:"SUT_name"`
-	SUT_version string `json:"SUT_version"`
-	Minio_key string `json:"minio_key"`
-	Trial_id string `json:"trial_id"`
-	Experiment_id string `json:"experiment_id"`
-	Total_trials_num int `json:"total_trials_num"`
-	Collector_name string `json:"collector_name"`
-	}
-
-func signalOnKafka(minioKey string) {
-	totalTrials, _ := strconv.Atoi(os.Getenv("TOTAL_TRIALS_NUM"))
-	kafkaMsg := KafkaMessage{SUT_name: os.Getenv("SUT_NAME"), SUT_version: os.Getenv("SUT_VERSION"), Minio_key: minioKey, Trial_id: os.Getenv("TRIAL_ID"), Experiment_id: os.Getenv("EXPERIMENT_ID"), Total_trials_num: totalTrials, Collector_name: os.Getenv("COLLECTOR_NAME")}
-	jsMessage, err := json.Marshal(kafkaMsg)
-	if err != nil {
-		log.Printf("Failed to marshall json message")
-		}
-	producer, err := sarama.NewSyncProducer([]string{os.Getenv("KAFKA_HOST")+":9092"}, nil)
-	if err != nil {
-	    log.Fatalln(err)
-	}
-	defer func() {
-	    if err := producer.Close(); err != nil {
-	        log.Fatalln(err)
-	    }
-	}()
-	msg := &sarama.ProducerMessage{Topic: os.Getenv("KAFKA_TOPIC"), Value: sarama.StringEncoder(jsMessage)}
-	partition, offset, err := producer.SendMessage(msg)
-	if err != nil {
-	    log.Printf("FAILED to send message: %s\n", err)
-	    } else {
-	    log.Printf("> message sent to partition %d at offset %d\n", partition, offset)
-	    }
-	}
  
 func backupHandler(w http.ResponseWriter, r *http.Request) {
     ev := os.Getenv("TO_ZIP")
     paths := strings.Split(ev, ",")
+    minioKey := minio.GenerateKey("folders", os.Getenv("BENCHFLOW_TRIAL_ID"), os.Getenv("BENCHFLOW_EXPERIMENT_ID"), os.Getenv("BENCHFLOW_CONTAINER_NAME"), os.Getenv("BENCHFLOW_COLLECTOR_NAME"), os.Getenv("BENCHFLOW_DATA_NAME"))
     for _, each := range paths {
         fmt.Fprintf(w, "Trying to zip %s\n", each)
         folderList := strings.Split(each, "/")
@@ -62,32 +24,16 @@ func backupHandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err)
 			}  
-	    minioKey := minio.GenerateKey(folderName+".tar.gz")
-		callMinioClient(folderName+".tar.gz", os.Getenv("MINIO_ALIAS"), minioKey)
+	    //minioKey := minio.GenerateKey(folderName+".tar.gz")
+		//callMinioClient(folderName+".tar.gz", os.Getenv("MINIO_ALIAS"), minioKey)
+		minio.SendGzipToMinio(folderName+".tar.gz", os.Getenv("MINIO_HOST"), os.Getenv("MINIO_PORT"), minioKey+folderName+".tar.gz", os.Getenv("MINIO_ACCESSKEYID"), os.Getenv("MINIO_SECRETACCESSKEY"))
 	    //minio.StoreOnMinio(folderName+".tar.gz", "runs", minioKey)
-	    signalOnKafka(minioKey)
 	}
+    kafka.SignalOnKafka(minioKey, os.Getenv("BENCHFLOW_TRIAL_ID"), os.Getenv("BENCHFLOW_EXPERIMENT_ID"), "files", "host", os.Getenv("BENCHFLOW_COLLECTOR_NAME"), os.Getenv("KAFKA_HOST"), os.Getenv("KAFKA_PORT"), os.Getenv("KAFKA_TOPIC"))
 	fmt.Fprintf(w, "SUCCESS")
 }
 
-func callMinioClient(fileName string, minioHost string, minioKey string) {
-		//TODO: change, we are using sudo to elevate the priviledge in the container, but it is not nice
-		//NOTE: it seems that the commands that are not in PATH, should be launched using sh -c
-		log.Printf("sh -c sudo /app/mc --quiet cp " + fileName + " " + minioHost + "/runs/" + minioKey)
-		cmd := exec.Command("sh", "-c", "sudo /app/mc --quiet cp " + fileName + " " + minioHost + "/runs/" + minioKey)
-    	var out bytes.Buffer
-		var stderr bytes.Buffer
-		cmd.Stdout = &out
-		cmd.Stderr = &stderr
-		err := cmd.Run()
-		if err != nil {
-		    fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-		    return
-		}
-		fmt.Println("Result: " + out.String())
-	}
- 
 func main() {
-    http.HandleFunc("/data", backupHandler)
+    http.HandleFunc("/store", backupHandler)
     http.ListenAndServe(":8080", nil)
 }
