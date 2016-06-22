16b2f99316d865c98ca7913c35a9b6949eee1d57
Gabriele Cerfoglio <gabrielecerfoglio225@gmail.com>
2016-06-22 15:46:16 +0200

    Commons for properties collector (#69)
    * commons, Minio API
    
    * deps
    
    * kafka from commons in makefile
    
    * port added to minio calls
    
    * single host, vendored commons minio correction

--- environment/properties/Godeps/_workspace/src/github.com/benchflow/commons/minio/minio.go
+++ environment/properties/Godeps/_workspace/src/github.com/benchflow/commons/minio/minio.go
@@ -1,16 +1,63 @@
 package minio
 
 import (
-	"os"
+	"github.com/minio/minio-go"
 	"os/exec"
-	"strings"
+	"fmt"
+	"crypto/md5"
+	"encoding/hex"
 )
 
-func GenerateKey(fileName string) string {
-	// TODO: Use the hash library to generate a hash
-	trialId := os.Getenv("TRIAL_ID")
-	t := strings.Split(trialId, "_")
-	return ("hash/"+t[0]+"/"+t[1]+"/"+os.Getenv("CONTAINER_NAME")+"/"+os.Getenv("COLLECTOR_NAME")+"/"+os.Getenv("DATA_NAME")+"/"+fileName)
+const numOfHashCharacters int = 4
+
+/*
+// We keep thos function in case the API fails because of a Minio update
+func callMinioClient(fileName string, minioHost string, minioKey string) {
+		//TODO: change, we are using sudo to elevate the priviledge in the container, but it is not nice
+		//NOTE: it seems that the commands that are not in PATH, should be launched using sh -c
+		log.Printf("sh -c sudo /app/mc --quiet cp " + fileName + " " + minioHost + "/runs/" + minioKey)
+		cmd := exec.Command("sh", "-c", "sudo /app/mc --quiet cp " + fileName + " " + minioHost + "/runs/" + minioKey)
+    	var out bytes.Buffer
+		var stderr bytes.Buffer
+		cmd.Stdout = &out
+		cmd.Stderr = &stderr
+		err := cmd.Run()
+		if err != nil {
+		    fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
+		    return
+		}
+		fmt.Println("Result: " + out.String())
+}
+*/
+
+func SendGzipToMinio(fileName string, minioHost string, minioPort string, minioKey string, accessKey string, secretAccessKey string) {
+	sendToMinio(fileName, minioHost, minioPort, minioKey, accessKey, secretAccessKey, "application/gzip")
+	}
+
+func sendToMinio(fileName string, minioHost string, minioPort string, minioKey string, accessKey string, secretAccessKey string, fileType string) error {
+	minioClient, err := minio.New(minioHost+":"+minioPort, accessKey, secretAccessKey, false)
+	if err != nil {
+		fmt.Println(err)
+	    return err
+	    }
+	_, err = minioClient.FPutObject("runs", minioKey, fileName, fileType)
+	if err != nil {
+	    fmt.Println(err)
+	    return err
+		}
+	return nil
+	}
+
+func hashKey(key string) string {
+	hasher := md5.New()
+    hasher.Write([]byte(key))
+    hashString := hex.EncodeToString(hasher.Sum(nil))
+	return (hashString[:numOfHashCharacters]+"/"+key)
+	}
+
+func GenerateKey(fileName string, trialID string, experimentID string, containerName string, collectorName string, dataName string) string {
+	key := experimentID+"/"+trialID+"/"+containerName+"/"+collectorName+"/"+dataName+"/"+fileName
+	return hashKey(key)
 }
 
 func GzipFile(fileName string) {
@@ -21,4 +68,3 @@ func GzipFile(fileName string) {
 		panic(err)
 		}
 }
-
--- environment/properties/src/cloud/benchflow/collectors/properties.go
+++ environment/properties/src/cloud/benchflow/collectors/properties.go
@@ -5,59 +5,13 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
-	"bytes"
 	"strings"
 	"github.com/fsouza/go-dockerclient"
-	"github.com/Shopify/sarama"
-	"encoding/json"
 	"github.com/benchflow/commons/minio"
-	"strconv"
+	"github.com/benchflow/commons/kafka"
 )
 
-type KafkaMessage struct {
-	SUT_name string `json:"SUT_name"`
-	SUT_version string `json:"SUT_version"`
-	Minio_key string `json:"minio_key"`
-	Trial_id string `json:"trial_id"`
-	Experiment_id string `json:"experiment_id"`
-	Container_id string `json:"container_id"`
-	Total_trials_num int `json:"total_trials_num"`
-	Collector_name string `json:"collector_name"`
-	}
-
-func signalOnKafka(minioKey string, containerID string) {
-	totalTrials, _ := strconv.Atoi(os.Getenv("BENCHFLOW_TRIAL_TOTAL_NUM"))
-	kafkaMsg := KafkaMessage{SUT_name: os.Getenv("SUT_NAME"), SUT_version: os.Getenv("SUT_VERSION"), Minio_key: minioKey, Trial_id: os.Getenv("BENCHFLOW_TRIAL_ID"), Experiment_id: os.Getenv("BENCHFLOW_EXPERIMENT_ID"), Container_id: containerID, Total_trials_num: totalTrials, Collector_name: os.Getenv("BENCHFLOW_COLLECTOR_NAME")}
-	jsMessage, err := json.Marshal(kafkaMsg)
-	if err != nil {
-		log.Printf("Failed to marshall json message")
-		}
-	producer, err := sarama.NewSyncProducer([]string{os.Getenv("KAFKA_HOST")+":9092"}, nil)
-	if err != nil {
-	    log.Fatalln(err)
-	}
-	defer func() {
-	    if err := producer.Close(); err != nil {
-	        log.Fatalln(err)
-	    }
-	}()
-	msg := &sarama.ProducerMessage{Topic: os.Getenv("KAFKA_TOPIC"), Value: sarama.StringEncoder(jsMessage)}
-	partition, offset, err := producer.SendMessage(msg)
-	if err != nil {
-	    log.Printf("FAILED to send message: %s\n", err)
-	    } else {
-	    log.Printf("> message sent to partition %d at offset %d\n", partition, offset)
-	    }
-	}
-
 func createDockerClient() docker.Client {
-	//path := os.Getenv("DOCKER_CERT_PATH")
-	//endpoint := "tcp://192.168.99.100:2376"
-    //ca := fmt.Sprintf("%s/ca.pem", path)
-    //cert := fmt.Sprintf("%s/cert.pem", path)
-    //key := fmt.Sprintf("%s/key.pem", path)
-    //client, err := docker.NewTLSClient(endpoint, cert, key, ca)
 	endpoint := "unix:///var/run/docker.sock"
     client, err := docker.NewClient(endpoint)
 	if err != nil {
@@ -68,10 +22,22 @@ func createDockerClient() docker.Client {
 
 func storeData(w http.ResponseWriter, r *http.Request) {
 	client := createDockerClient()
+	
+	info, err := client.Info()
+	if err != nil {
+		panic(err)
+		}
+	
+	version, err := client.Version()
+	if err != nil {
+		panic(err)
+		}
+	
 	contEV := os.Getenv("CONTAINERS")
 	conts := strings.Split(contEV, ",")
 	composedMinioKey := ""
 	composedContainerIds := ""
+	hostID := info.ID
 	for _, each := range conts {
 		var e docker.Env
 		
@@ -95,17 +61,9 @@ func storeData(w http.ResponseWriter, r *http.Request) {
 		e.SetJSON("inspect", inspect)
 		foInspect.Write([]byte(e.Get("inspect")))
 		
-		info, err := client.Info()
-		if err != nil {
-			panic(err)
-			}
 		e.SetJSON("info", info)
 		foInfo.Write([]byte(e.Get("info")))
 		
-		version, err := client.Version()
-		if err != nil {
-			panic(err)
-			}
 		e.SetJSON("version", version)
 		foVersion.Write([]byte(e.Get("version")))
 		
@@ -113,15 +71,19 @@ func storeData(w http.ResponseWriter, r *http.Request) {
 		minio.GzipFile("/app/"+each+"_info_tmp")
 		minio.GzipFile("/app/"+each+"_version_tmp")
 		
-		minioKey := minio.GenerateKey(each)
+		//minioKey := minio.GenerateKey(each)
+		minioKey := minio.GenerateKey(each, os.Getenv("BENCHFLOW_TRIAL_ID"), os.Getenv("BENCHFLOW_EXPERIMENT_ID"), os.Getenv("BENCHFLOW_CONTAINER_NAME"), os.Getenv("BENCHFLOW_COLLECTOR_NAME"), os.Getenv("BENCHFLOW_DATA_NAME"))
 		composedMinioKey = composedMinioKey+minioKey+","
 		composedContainerIds = composedContainerIds+inspect.ID+","
 		
 		fmt.Println(minioKey)
 		
-		callMinioClient("/app/"+each+"_inspect_tmp.gz", os.Getenv("MINIO_ALIAS"), minioKey+"_inspect.gz")
-		callMinioClient("/app/"+each+"_info_tmp.gz", os.Getenv("MINIO_ALIAS"), minioKey+"_info.gz")
-		callMinioClient("/app/"+each+"_version_tmp.gz", os.Getenv("MINIO_ALIAS"), minioKey+"_version.gz")
+		//callMinioClient("/app/"+each+"_inspect_tmp.gz", os.Getenv("MINIO_ALIAS"), minioKey+"_inspect.gz")
+		minio.SendGzipToMinio("/app/"+each+"_inspect_tmp.gz", os.Getenv("MINIO_HOST"), os.Getenv("MINIO_PORT"), minioKey+"_inspect.gz", os.Getenv("MINIO_ACCESSKEYID"), os.Getenv("MINIO_SECRETACCESSKEY"))
+		//callMinioClient("/app/"+each+"_info_tmp.gz", os.Getenv("MINIO_ALIAS"), minioKey+"_info.gz")
+		minio.SendGzipToMinio("/app/"+each+"_info_tmp.gz", os.Getenv("MINIO_HOST"), os.Getenv("MINIO_PORT"), minioKey+"_info.gz", os.Getenv("MINIO_ACCESSKEYID"), os.Getenv("MINIO_SECRETACCESSKEY"))
+		//callMinioClient("/app/"+each+"_version_tmp.gz", os.Getenv("MINIO_ALIAS"), minioKey+"_version.gz")
+		minio.SendGzipToMinio("/app/"+each+"_version_tmp.gz", os.Getenv("MINIO_HOST"), os.Getenv("MINIO_PORT"), minioKey+"_version.gz", os.Getenv("MINIO_ACCESSKEYID"), os.Getenv("MINIO_SECRETACCESSKEY"))
 		
 		err = os.Remove("/app/"+each+"_inspect_tmp.gz")
 		if err != nil {
@@ -139,24 +101,8 @@ func storeData(w http.ResponseWriter, r *http.Request) {
 	composedMinioKey = strings.TrimRight(composedMinioKey, ",")
 	composedContainerIds = strings.TrimRight(composedContainerIds, ",")
 	fmt.Println(composedMinioKey)
-	signalOnKafka(composedMinioKey, composedContainerIds)
-	}
-
-func callMinioClient(fileName string, minioHost string, minioKey string) {
-		//TODO: change, we are using sudo to elevate the priviledge in the container, but it is not nice
-		//NOTE: it seems that the commands that are not in PATH, should be launched using sh -c
-		log.Printf("sh -c sudo /app/mc --quiet cp " + fileName + " " + minioHost + "/runs/" + minioKey)
-		cmd := exec.Command("sh", "-c", "sudo /app/mc --quiet cp " + fileName + " " + minioHost + "/runs/" + minioKey)
-    	var out bytes.Buffer
-		var stderr bytes.Buffer
-		cmd.Stdout = &out
-		cmd.Stderr = &stderr
-		err := cmd.Run()
-		if err != nil {
-		    fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-		    return
-		}
-		fmt.Println("Result: " + out.String())
+	//kafka.SignalOnKafka(composedMinioKey, composedContainerIds)
+	kafka.SignalOnKafka(composedMinioKey, os.Getenv("BENCHFLOW_TRIAL_ID"), os.Getenv("BENCHFLOW_EXPERIMENT_ID"), composedContainerIds, hostID, os.Getenv("BENCHFLOW_COLLECTOR_NAME"), os.Getenv("KAFKA_HOST"), os.Getenv("KAFKA_PORT"), os.Getenv("KAFKA_TOPIC"))
 	}
 
 func main() {
